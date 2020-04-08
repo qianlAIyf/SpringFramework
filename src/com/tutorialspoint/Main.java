58b20130ff86fe533dd79dddcc170228e4aee4cb
qianlAIyf <1769234376@qq.com>
2020-04-08 21:00:48 +0800

    .

--- src/com/tutorialspoint/Main.java
+++ src/com/tutorialspoint/Main.java
@@ -2,11 +2,13 @@ package com.tutorialspoint;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Main {
 
 	public static void main(String[] args) {
-		ApplicationContext content = new ClassPathXmlApplicationContext("Beans.xml");
+//		ApplicationContext content = new ClassPathXmlApplicationContext("Beans.xml");   //use ClassPathXmlApplicationContext
+		ApplicationContext content = new FileSystemXmlApplicationContext("D:/codesoftwares/JAVA/project/Spring/src/Beans.xml");	//use FileSystemXmlApplicationContext
 		HelloWorld helloWorld = (HelloWorld) content.getBean("HelloWorld");
 		helloWorld.getMessage();
 	}
