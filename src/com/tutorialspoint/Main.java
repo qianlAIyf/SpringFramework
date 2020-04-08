6d513043444e158622cd2630a1ff52f0229b9d6f
qianlAIyf <1769234376@qq.com>
2020-04-08 20:17:26 +0800

    hello world

---
+++ src/com/tutorialspoint/Main.java
@@ -0,0 +1,14 @@
+package com.tutorialspoint;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ApplicationContext content = new ClassPathXmlApplicationContext("Beans.xml");
+		HelloWorld helloWorld = (HelloWorld) content.getBean("HelloWorld");
+		helloWorld.getMessage();
+	}
+
+}
